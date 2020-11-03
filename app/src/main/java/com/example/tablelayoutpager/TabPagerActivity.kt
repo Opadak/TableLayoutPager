5f677bed2cc9858f5f6d1607cc6dc533565e9166
Opadak <tfk1019@naver.com>
2020-11-04 00:08:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tablelayoutpager/TabPagerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tablelayoutpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabPagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_pager)
+    }
+}
+
